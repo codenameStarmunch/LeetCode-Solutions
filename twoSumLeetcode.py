16f09d53aa2fa99ea0cb1607bd1a912c6c0a3bd4
Pete Kai Cunliffe <90422330+codenameStarmunch@users.noreply.github.com>
2022-05-05 12:25:38 +0100

    Add files via upload

---
+++ twoSumLeetcode.py
@@ -0,0 +1,13 @@
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        result = []
+
+        for x in range(len(nums)):
+            for y in range(x + 1, len(nums)):
+                if nums[x] + nums[y] == target:
+                    result.append(x)
+                    result.append(y)
+
+        return result
